080eaeb9840380e2d5832cd6e1256e2667f10e01
Stephen Decker <sdeckerm@gmail.com>
2017-11-01 21:00:14 +0000

    added cli class

--- club_nights/lib/club_nights.rb
+++ club_nights/lib/club_nights.rb
@@ -1,5 +1,6 @@
-require "club_nights/version"
+require_relative "./club_nights/version"
+require_relative "./club_nights/cli"
 
-module ClubNights
-  # Your code goes here...
-end
+# module ClubNights
+#   # Your code goes here...
+# end
