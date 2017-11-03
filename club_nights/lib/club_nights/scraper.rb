bf9517dcd7e62dae2d2577a3f0979ee7c7f8b3bd
Stephen Decker <sdeckerm@gmail.com>
2017-11-03 00:10:53 +0000

    added scraper method to create hash from region list + provide country/city variables

--- club_nights/lib/club_nights/scraper.rb
+++ club_nights/lib/club_nights/scraper.rb
@@ -12,4 +12,11 @@ class ClubNights::Scraper
     end
   end
 
+  def self.scrape_city_country(input)
+    strip_location = Hash[*scrape_region_list.collect(&:strip).map {|word| "#{word},"}.join.gsub(/[ + ]/, '').split(',')]
+
+    @city = strip_location.map {|k,v| k.downcase }[input.to_i - 1]
+    @country = strip_location.map {|k,v| v.downcase }[input.to_i - 1]
+  end
+
 end
